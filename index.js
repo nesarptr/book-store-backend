17d94d182e308241fdf2833f3382c009f5305740
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2022-12-21 19:59:21 +0600

    Basic Setup Done

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use("/", (_, res) => {
+  res.status(200).json({
+    message: `Hello World`,
+  });
+});
+
 app.listen(PORT);
